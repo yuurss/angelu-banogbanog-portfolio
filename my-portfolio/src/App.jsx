51395463b83d894ef348d60f48bee562bc101a6a
yuurss <155753158+yuurss@users.noreply.github.com>
2025-10-15 14:26:04 +0800

    Update App.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -8,6 +8,7 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+<meta name="google-site-verification" content="beP4S0PQQ9GunJ1ly5M8z5MnnLaVj9FnexIf9Ya5neA" />
 function App() {
   
 
