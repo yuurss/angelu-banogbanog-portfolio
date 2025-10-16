6e9962fc1756f326fd30d2b4a3d87d40993290ce
yuurss <155753158+yuurss@users.noreply.github.com>
2025-10-16 13:56:18 +0800

    modified

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
+import tailwindcss from '@tailwindcss/vite'
+import vitePluginSitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(),
+  plugins: [
+    tailwindcss(),
+    react(),
+    vitePluginSitemap({
+      hostname: 'https://angelubanogbanog.github.io/angelu-banogbanog-portfolio', // your live site
+      outDir: 'dist',
+    }),
   ],
-  base: '/angelu-banogbanog-portfolio/', 
+  base: '/angelu-banogbanog-portfolio/',
 })
